ac8a4b277548624d9a1acf21079a3f36372fda38
IaraUriarte <iaraherlein0001@gmail.com>
2026-05-17 12:48:52 -0300

    Avance en el Ej20

--- TP2.py
+++ TP2.py
@@ -3,3 +3,15 @@
 class stack:
     def __init__(self):
         self.__elements = []
+
+    def push(self, value):
+        self.__elements.append(value)
+
+    def pop(self):
+        return self.__elements.pop()
+    
+    def size(self):
+        return len(self.__elements)
+    
+    def show(self):
+        print(self.__elements)
