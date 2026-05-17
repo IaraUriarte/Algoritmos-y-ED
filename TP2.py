a399b90684da570aa9645e304e50f70b72dd9761
IaraUriarte <iaraherlein0001@gmail.com>
2026-05-17 15:38:07 -0300

    Avance en el Ej20

--- TP2.py
+++ TP2.py
@@ -24,11 +24,18 @@ class stack:
 
         while True:
             entrada_pasos = input("Cantidad de pasos ('fin' para terminar): ").strip().lower()
-            if entrada_pasos == 'fin'
-            }   break
+            if entrada_pasos == 'fin':
+                break
 
             try:
                 pasos = int(entrada_pasos)
             except(ValueError):
                 print("Ingrese un número válido")
+                continue
+
+            direc = input("Dirección: ").strip().lower()
+            direc_validas = ['Norte', 'Noreste', 'Este', 'Sureste', 'Sur', 'Suroeste', 'Oeste', 'Noroeste']
+
+            if direc not in direc_validas:
+                print(f"Dirección '{direc}' no válida. Intente de nuevo")
                 continue
