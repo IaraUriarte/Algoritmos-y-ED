810d78b3167c39fe2ac5adc97ffdf3629911570d
IaraUriarte <iaraherlein0001@gmail.com>
2026-05-17 15:30:05 -0300

    Avance en el Ej20

--- TP2.py
+++ TP2.py
@@ -15,3 +15,20 @@ class stack:
     
     def show(self):
         print(self.__elements)
+
+    def reg_mov():
+        pila_mov = stack()
+        print("Anotador de los pasos del robot")
+        print("Opciones de dirección: Norte, Noreste, Este, Sureste, Sur, Suroeste, Oeste, Noroeste")
+        print("Escriba 'fin' en la cantidad de pasos para terminar el registro.\n")
+
+        while True:
+            entrada_pasos = input("Cantidad de pasos ('fin' para terminar): ").strip().lower()
+            if entrada_pasos == 'fin'
+            }   break
+
+            try:
+                pasos = int(entrada_pasos)
+            except(ValueError):
+                print("Ingrese un número válido")
+                continue
